4e803edcb97df80543fa637e0149495f4efe73c0
Carlo Askander <carloaskander@live.se>
2024-01-16 01:00:16 +0100

    Fix incorrect file path causing deployment failure

--- src/components/booknow/Booknow.jsx
+++ src/components/booknow/Booknow.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import './Booknow.css';
-// Add any other imports you need here
+import './BookNow.css';
 
 function BookNow() {
   return (
     <div>
-      {/* Add your BookNow component's content and logic here */}
     </div>
   );
 }
