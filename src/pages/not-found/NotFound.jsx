e8248289451bf22955a7464a2da73c0840a47270
Carlo Askander <carloaskander@live.se>
2024-06-13 02:05:18 +0200

    made the same changes to the buttons on the 404 not found page as well as added translations for that page in all languages provided on the website.

--- src/pages/not-found/NotFound.jsx
+++ src/pages/not-found/NotFound.jsx
@@ -1,32 +1,40 @@
 import React from 'react';
-import { Box, Container, Typography, Button } from '@mui/material';
+import { useTranslation } from 'react-i18next';
+import { Box, Typography, Button, Container } from '@mui/material';
 import HomeIcon from '@mui/icons-material/Home';
 import ExploreIcon from '@mui/icons-material/Explore';
 import { useNavigate } from 'react-router-dom';
 
 function NotFound() {
+    const { t, i18n } = useTranslation();
     const navigate = useNavigate();
+    const currentLanguage = i18n.language;
+
+    const handleNavigate = (path) => {
+        navigate(`/${currentLanguage}${path}`);
+    };
 
     return (
         <Box sx={{ bgcolor: '#FAF6EE', minHeight: 'calc(100vh - 200px)', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-        <Container maxWidth="sm" sx={{ textAlign: 'center' }}>
-            <Typography variant="h2" gutterBottom>
-            404 - Sidan hittades inte
-            </Typography>
-            <Typography variant="subtitle1" mb={4}>
-            Tyvärr kan vi inte hitta den sida du letar efter. Det kan bero på att sidan inte finns längre eller att en felaktig URL har angetts.
-            </Typography>
-            <Typography variant="body1">
-            En flitig programmerare sitter och jobbar för fullt bakom kulisserna för att kontinuerligt förbättra vår webbplats och ge dig en bättre användarupplevelse. Du kanske hittar sidan du letade efter genom att navigera på vår webbplats.</Typography>
-            <Box mt={4}>
-            <Button variant="outlined" startIcon={<HomeIcon />} onClick={() => navigate('/home')}>
-                Hem
-            </Button>
-            <Button variant="outlined" startIcon={<ExploreIcon />} sx={{ ml: 2 }} onClick={() => navigate('/accommodation/camping')}>
-                Camping
-            </Button>
-            </Box>
-        </Container>
+            <Container maxWidth="sm" sx={{ textAlign: 'center' }}>
+                <Typography variant="h2" gutterBottom>
+                    404 - {t('notFoundPage.title')}
+                </Typography>
+                <Typography variant="subtitle1" mb={4}>
+                    {t('notFoundPage.subtitle')}
+                </Typography>
+                <Typography variant="body1">
+                    {t('notFoundPage.description')}
+                </Typography>
+                <Box mt={4}>
+                    <Button variant="outlined" startIcon={<HomeIcon />} onClick={() => handleNavigate('/home')}>
+                        {t('notFoundPage.homeButton')}
+                    </Button>
+                    <Button variant="outlined" startIcon={<ExploreIcon />} sx={{ ml: 2 }} onClick={() => handleNavigate('/accommodation/camping')}>
+                        {t('notFoundPage.campingButton')}
+                    </Button>
+                </Box>
+            </Container>
         </Box>
     );
 }
